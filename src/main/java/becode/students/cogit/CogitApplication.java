5bd65f61d646c36ae2902c6f5af508e4a3dcca2c
MaTor97 <mttortora97@gmail.com>
2025-01-29 14:38:58 +0100

    first commit

---
+++ src/main/java/becode/students/cogit/CogitApplication.java
@@ -0,0 +1,13 @@
+package becode.students.cogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/becode/students/cogit/CogitApplicationTests.java
@@ -0,0 +1,13 @@
+package becode.students.cogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
