b43f65ef40d9146b56871b87c90a0f4422b91bf5
Renaud L <lefebvre.renaud27@gmail.com>
2025-02-07 15:23:08 +0100

    Invoices Last5 and Page done

--- src/main/java/becode/students/cogit/Invoice/InvoiceRepository.java
+++ src/main/java/becode/students/cogit/Invoice/InvoiceRepository.java
@@ -1,5 +1,8 @@
 package becode.students.cogit.Invoice;
 
+import becode.students.cogit.Contacts.Contact;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +13,5 @@ public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
     Optional<Invoice> findById(int id);
     Optional<Invoice> findByCompanyId(int id_company);
     Optional<Invoice> findByRef(String ref);
+    Optional<Invoice> findTop5ByOrderByCreatedAtDesc();
 }
